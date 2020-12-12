6d5cc4655c044548037efc0469d085eec8a73ce0
OveR1st <315overdose@gmail.com>
2020-12-12 22:32:43 +0200

    Action Creator

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const reducer = (state = 0, action) => {
     case 'DEC':
       return state - 1;
 
+    case 'RND':
+      return state + action.payload;
+
     default:
       return state;
   }
@@ -17,15 +20,27 @@ const reducer = (state = 0, action) => {
 
 const store = createStore(reducer);
 
+const inc = () => ({type: 'INC'});
+
+const dec = () => ({type: 'DEC'});
+
+const rnd = (payload) => ({ type: 'RND', payload });
+
 document
   .getElementById('inc')
   .addEventListener('click', () => {
-    store.dispatch({type: 'INC'});
+    store.dispatch(inc());
   });
 document
   .getElementById('dec')
   .addEventListener('click', () => {
-    store.dispatch({type: 'DEC'});
+    store.dispatch(dec());
+  });
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random()*10);
+    store.dispatch(rnd(payload));
   });
 
   const update = () => {
