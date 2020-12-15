480370d7c5c2f4574789e093ccd3eb2a3c56b844
OveR1st <315overdose@gmail.com>
2020-12-15 12:25:32 +0200

    final version counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
+// import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
@@ -12,13 +12,13 @@ const Counter = ({ counter, inc, dec, rnd }) => {
       <h2>{counter}</h2>
       <button
         onClick={dec}
-        class="btn btn-primary btn-lg">DEC</button>
+        className="btn btn-primary btn-lg">DEC</button>
       <button 
         onClick={inc}
-        class="btn btn-primary btn-lg">INC</button>
+        className="btn btn-primary btn-lg">INC</button>
       <button
         onClick={rnd} 
-        class="btn btn-primary btn-lg">RND</button>
+        className="btn btn-primary btn-lg">RND</button>
     </div>
   );
 }
